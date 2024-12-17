861be2ce195dbc7f218260930d055ffe91738068
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-17 23:52:00 +0630

    feat : CustomerList api and create customer api

---
+++ DotNetCoreRESTApi.MiniKpay/Controllers/Customer/CustomerController.cs
@@ -0,0 +1,37 @@
+﻿using DotNetCoreRESTApi.MiniKpay.BusinessLogic.Features.Customer;
+using DotNetCoreRESTApi.MiniKpay.Database.Models;
+using DotNetCoreRESTApi.MiniKpay.Database.RequestModel.Customer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreRESTApi.MiniKpay.Controllers.Customer
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly CustomerService _customerService = new CustomerService();
+
+        [HttpGet]
+        public IActionResult GetCustomers()
+        {
+            var customerList = _customerService.GetCustomers();
+            if (customerList.Count is 0)
+            {
+                return Ok(new { message = "CustomerList is Empty"});
+            }
+            return Ok(customerList);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCustomer(CustomerRequestModel reqModel)
+        {
+            var result = _customerService.CreateCustomer(reqModel);
+            if (result is 0)
+            {
+                return BadRequest(new { message = "Customer Creation Fail" });
+            }
+            return Ok(new { message = "Customer Creation Successful" });
+        }
+    }
+}
