7bc21e73ca55177308f1c83ac599eaa1e45f5d96
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-17 23:51:44 +0630

    feat : get Customer List and Create Customer

---
+++ DotNetCoreRESTApi.MiniKpay.BusinessLogic/Features/Customer/CustomerService.cs
@@ -0,0 +1,32 @@
+﻿using DotNetCoreRESTApi.MiniKpay.Database.Models;
+using DotNetCoreRESTApi.MiniKpay.Database.RequestModel.Customer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetCoreRESTApi.MiniKpay.BusinessLogic.Features.Customer;
+
+public class CustomerService
+{
+    private readonly AppDbContext _db = new AppDbContext();
+
+    public List<TblCustomer> GetCustomers()
+    {
+       var lst =  _db.TblCustomers.ToList();
+        return lst;
+    }
+
+    public int CreateCustomer(CustomerRequestModel reqModel)
+    {
+        _db.Add(new TblCustomer
+        {
+            CustomerCode = Ulid.NewUlid().ToString(),
+            CustomerName = reqModel.CustomerName,
+            MobileNo = reqModel.MobileNo,
+        });
+        var result = _db.SaveChanges();
+        return result;
+    }
+}
