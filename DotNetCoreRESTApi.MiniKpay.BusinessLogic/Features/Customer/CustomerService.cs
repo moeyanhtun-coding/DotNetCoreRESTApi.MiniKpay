a72cbc4137cdb462be65ba94b374f5199ae1c508
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-18 11:17:10 +0630

    feat : fix add customer and CustomerBalance and PinCode Hashed

--- DotNetCoreRESTApi.MiniKpay.BusinessLogic/Features/Customer/CustomerService.cs
+++ DotNetCoreRESTApi.MiniKpay.BusinessLogic/Features/Customer/CustomerService.cs
@@ -1,10 +1,8 @@
-﻿using DotNetCoreRESTApi.MiniKpay.Database.Models;
+﻿using System.Security.Cryptography;
+using DotNetCoreRESTApi.MiniKpay.Database.Models;
 using DotNetCoreRESTApi.MiniKpay.Database.RequestModel.Customer;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.EntityFrameworkCore;
 
 namespace DotNetCoreRESTApi.MiniKpay.BusinessLogic.Features.Customer;
 
@@ -14,19 +12,77 @@ public class CustomerService
 
     public List<TblCustomer> GetCustomers()
     {
-       var lst =  _db.TblCustomers.ToList();
+        var lst = _db.TblCustomers.ToList();
         return lst;
     }
 
     public int CreateCustomer(CustomerRequestModel reqModel)
     {
-        _db.Add(new TblCustomer
+        string customerCode = Ulid.NewUlid().ToString();
+        byte[] salt = RandomNumberGenerator.GetBytes(128 / 8);
+        var hashed = HashValue(reqModel.PinCode, salt);
+        _db.TblCustomers.Add(new TblCustomer
         {
-            CustomerCode = Ulid.NewUlid().ToString(),
+            CustomerCode = customerCode,
             CustomerName = reqModel.CustomerName,
             MobileNo = reqModel.MobileNo,
+            PinCode = hashed,
+            Salt = Convert.ToBase64String(salt)
+        });
+        _db.TblCustomerBalances.Add(new TblCustomerBalance
+        {
+            CustomerCode = customerCode,
+            Balance = 10000
         });
         var result = _db.SaveChanges();
         return result;
+    }
+
+    public int UpdatePinCode(string customerCode, PinCodeRequestModel reqModel)
+    {
+        try
+        {
+            var item = _db.TblCustomers.AsNoTracking().FirstOrDefault(x => x.CustomerCode == customerCode)!;
+            var validatePinCode = VerifyPinCode(reqModel.oldPinCode, item.PinCode, item.Salt);
+            if (!validatePinCode)
+                throw new ArgumentException("PinCode Changing Failed");
+            byte[] salt = RandomNumberGenerator.GetBytes(128 / 8);
+            var hashed = HashValue(reqModel.newPinCode, salt);
+
+            item.Salt = Convert.ToBase64String(salt);
+            item.PinCode = hashed;
+            _db.Entry(item).State = EntityState.Modified;
+            var result = _db.SaveChanges();
+            return result;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.ToString());
+        }
+    }
+
+    public string HashValue(string pinCode, byte[] salt)
+    {
+        string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: pinCode,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 100000,
+            numBytesRequested: 256 / 8
+            ));
+        return hashed;
+    }
+
+    public bool VerifyPinCode(string oldPinCode, string storedHash, string storedSalt)
+    {
+        byte[] salt = Convert.FromBase64String(storedSalt);
+        string hashedPinCode = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: oldPinCode,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 100000,
+ numBytesRequested: 256 / 8
+            ));
+        return hashedPinCode == storedHash;
     }
 }
