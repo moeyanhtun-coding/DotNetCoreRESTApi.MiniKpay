2fec10139aa7389f5465758632747e9220045e13
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-18 11:17:44 +0630

    feat : add resquest Model

--- DotNetCoreRESTApi.MiniKpay.Database/RequestModel/Customer/PinCodeRequestModel.cs
+++ DotNetCoreRESTApi.MiniKpay.Database/RequestModel/Customer/PinCodeRequestModel.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -8,15 +7,15 @@ using System.Threading.Tasks;
 
 namespace DotNetCoreRESTApi.MiniKpay.Database.RequestModel.Customer
 {
-    public class CustomerRequestModel
+    public class PinCodeRequestModel
     {
-        public string CustomerName { get; set; }
-        public string MobileNo { get; set; }
+        [Required(ErrorMessage ="Old PinCode is required")]
+        public string oldPinCode { get; set; }
 
-        [Required(ErrorMessage = "PinCode is required.")]
+        [Required(ErrorMessage = "New PinCode is required.")]
         [StringLength(6, ErrorMessage = "PinCode must be exactly 6 characters.")]
         [MinLength(6, ErrorMessage = "PinCode must be exactly 6 characters.")]
         [RegularExpression(@"^\d{6}$", ErrorMessage = "PinCode must contain exactly 6 digits.")]
-        public string PinCode { get; set; }
+        public string newPinCode { get; set; }
     }
 }
