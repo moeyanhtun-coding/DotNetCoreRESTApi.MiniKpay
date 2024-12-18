951de4bf9f89f3cc6a18c43f0e1ed1cfdad3a7ba
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-18 11:16:09 +0630

    feat : update Pincode for customer

--- DotNetCoreRESTApi.MiniKpay/Controllers/Customer/CustomerController.cs
+++ DotNetCoreRESTApi.MiniKpay/Controllers/Customer/CustomerController.cs
@@ -4,34 +4,51 @@ using DotNetCoreRESTApi.MiniKpay.Database.RequestModel.Customer;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DotNetCoreRESTApi.MiniKpay.Controllers.Customer
+namespace DotNetCoreRESTApi.MiniKpay.Controllers.Customer;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CustomerController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class CustomerController : ControllerBase
+    private readonly CustomerService _customerService = new CustomerService();
+
+    [HttpGet]
+    public IActionResult GetCustomers()
     {
-        private readonly CustomerService _customerService = new CustomerService();
-
-        [HttpGet]
-        public IActionResult GetCustomers()
+        var customerList = _customerService.GetCustomers();
+        if (customerList.Count is 0)
         {
-            var customerList = _customerService.GetCustomers();
-            if (customerList.Count is 0)
-            {
-                return Ok(new { message = "CustomerList is Empty"});
-            }
-            return Ok(customerList);
+            return Ok(new { message = "CustomerList is Empty"});
         }
+        return Ok(customerList);
+    }
 
-        [HttpPost]
-        public IActionResult CreateCustomer(CustomerRequestModel reqModel)
+    [HttpPost]
+    public IActionResult CreateCustomer(CustomerRequestModel reqModel)
+    {
+        var result = _customerService.CreateCustomer(reqModel);
+        if (result is 0)
         {
-            var result = _customerService.CreateCustomer(reqModel);
+            return BadRequest(new { message = "Customer Creation Fail" });
+        }
+        return Ok(new { message = "Customer Creation Successful" });
+    }
+
+    [HttpPatch("{customerCode}")]
+    public IActionResult UpdatePinCode(string customerCode, PinCodeRequestModel reqModel)
+    {
+        try
+        {
+            var result = _customerService.UpdatePinCode(customerCode, reqModel);
             if (result is 0)
             {
-                return BadRequest(new { message = "Customer Creation Fail" });
+                return BadRequest(new { message = "PinCode Update Fail" });
             }
-            return Ok(new { message = "Customer Creation Successful" });
+            return Ok(new { message = "PinCode Update Successful" });
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new { message = ex.ToString() });
         }
     }
 }
